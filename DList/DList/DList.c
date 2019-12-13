ab8bc034c3095936e6f893269a3239f5a63aec10
Ellyn-jia <15846706868@163.com>
2019-12-13 23:39:58 +0800

    .

--- DList/DList/DList.c
+++ DList/DList/DList.c
@@ -2,9 +2,26 @@
 
 ListNode* BuyNode(LTDataType x)
 {
-
+	ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+	newNode->data = x;
+	newNode->next = NULL;
+	newNode->prev = NULL;
+	return newNode;
 }
 ListNode* ListCreat()
 {
-
+	ListNode* head = (ListNode*)malloc(sizeof(ListNode));
+	head->next = head;
+	head->prev = head;
 }
+void ListDestory(ListNode* plist);
+void ListPrint(ListNode* plist);
+void ListPushBack(ListNode* plist, LTDataType x);
+void ListPushFront(ListNode* plist, LTDataType x);
+void ListPopBack(ListNode* plist);
+void ListPopFront(ListNode* plist);
+ListNode* ListFind(ListNode* plist, LTDataType x);
+void ListInsert(ListNode* pos, LTDataType x);
+void ListErase(ListNode* pos);
+
+
