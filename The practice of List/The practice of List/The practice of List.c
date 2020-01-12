771046f69066bd7f3b0105abc483fa2fc0f0f76c
Ellyn-jia <15846706868@163.com>
2020-01-12 22:59:35 +0800

    1_12 m

--- The practice of List/The practice of List/The practice of List.c
+++ The practice of List/The practice of List/The practice of List.c
@@ -154,6 +154,39 @@ public:
 
 
 
+//链表回文
 
+class PalindromeList {
+public:
+	bool chkPalindrome(ListNode* A) {
+		// write code here
 
-
+		if (A == NULL || A->next == NULL)
+			return true;      
+		ListNode* slow, *fast, *prev, *cur, *nxt;        
+		slow = fast = A;        
+		//找到中间节点        
+		while (fast && fast->next)
+		{            
+			slow = slow->next;            
+			fast = fast->next->next;        
+		}        prev = NULL;        
+		//后半部分逆置        
+		cur = slow;       
+		while (cur)       
+		{           
+			next = cur->next;            
+			cur->next = prev;            
+			prev = cur;            
+			cur = nxt;        
+		}       
+		//逐点比对        
+		while (A && prev)       
+		{            
+			if (A->val != prev->val)           
+				return false;       
+			A = A->next;         
+			prev = prev->next;   
+		} 
+	}
+};
