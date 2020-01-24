df9bb835996a51fca650fd5fb08f5b9cfd055a84
Ellyn-jia <15846706868@163.com>
2020-01-24 23:22:23 +0800

    1_24 c

--- The practice of List/The practice of List/The practice of List.c
+++ The practice of List/The practice of List/The practice of List.c
@@ -159,46 +159,12 @@ public:
 
 //链表回文
 
-class PalindromeList {
-public:
-	bool chkPalindrome(ListNode* A) {
-		// write code here
-
-		if (A == NULL || A->next == NULL)
-			return true;      
-		ListNode* slow, *fast, *prev, *cur, *nxt;        
-		slow = fast = A;        
-		//找到中间节点        
-		while (fast && fast->next)
-		{            
-			slow = slow->next;            
-			fast = fast->next->next;        
-		}        prev = NULL;        
-		//后半部分逆置        
-		cur = slow;       
-		while (cur)       
-		{           
-			next = cur->next;            
-			cur->next = prev;            
-			prev = cur;            
-			cur = nxt;        
-		}       
-		//逐点比对        
-		while (A && prev)       
-		{            
-			if (A->val != prev->val)           
-				return false;       
-			A = A->next;         
-			prev = prev->next;   
-		} 
-	}
-};
 
 
 
 
 
-
+//输入两个链表，找出它们的第一个公共结点
 
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
 	int lenA = 0, lenB = 0;    
