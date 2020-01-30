4bc8f613c8905b422ecf2fd2cf215b8f200348d0
Ellyn-jia <15846706868@163.com>
2020-01-30 22:45:47 +0800

    1_30 c

--- The practice of List/The practice of List/The practice of List.c
+++ The practice of List/The practice of List/The practice of List.c
@@ -209,46 +209,104 @@ public:
 
 //输入两个链表，找出它们的第一个公共结点
 
+//struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+//	int lenA = 0, lenB = 0;    
+//	struct ListNode* curA = headA, *curB = headB;    
+//	//计算链表长度   
+//	while(curA)
+//	{      
+//		++lenA;    
+//		curA = curA->next;   
+//	}       
+//	while(curB)
+//	{ 
+//		++lenB;     
+//		curB = curB->next;
+//	}       
+//	int gap = abs(lenA - lenB);    
+//	struct ListNode* longList = headA, *shortList = headB;   
+//	if (lenA < lenB) { longList = headB;      
+//	shortList = headA; }   
+//	//让长链表先走几步   
+//	while(gap--)
+//	{      
+//		longList = longList->next;  
+//	}    //两个链表同时走，直到遇到相同的节点   
+//	while(longList && shortList)  
+//	{      
+//		if(longList->val == shortList->val)
+//		{        
+//			return longList;       
+//		}        
+//		else 
+//		{    
+//			longList = longList->next;    
+//			shortList = shortList->next;   
+//		}  
+//	}    
+//	return NULL; 
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
 struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
-	int lenA = 0, lenB = 0;    
-	struct ListNode* curA = headA, *curB = headB;    
-	//计算链表长度   
-	while(curA)
-	{      
-		++lenA;    
-		curA = curA->next;   
-	}       
-	while(curB)
-	{ 
-		++lenB;     
+	struct ListNode* curA = headA;
+	struct ListNode* curB = headB;
+	int lenA = 0;
+	int lenB = 0;
+	while (curA)
+	{
+		lenA++;
+		curA = curA->next;
+	}
+	while (curB)
+	{
+		++lenB;
 		curB = curB->next;
-	}       
-	int gap = abs(lenA - lenB);    
-	struct ListNode* longList = headA, *shortList = headB;   
-	if (lenA < lenB) { longList = headB;      
-	shortList = headA; }   
-	//让长链表先走几步   
-	while(gap--)
-	{      
-		longList = longList->next;  
-	}    //两个链表同时走，直到遇到相同的节点   
-	while(longList && shortList)  
-	{      
-		if(longList->val == shortList->val)
-		{        
-			return longList;       
-		}        
-		else 
-		{    
-			longList = longList->next;    
-			shortList = shortList->next;   
-		}  
-	}    
-	return NULL; 
+	}
+	int value = abs(lenA - lenB);
+	struct ListNode* longList = headA;
+	struct ListNode* shortList = headB;
+	if (lenA < lenB)
+	{
+		struct ListNode* longList = headB;
+		struct ListNode* shortList = headA;
+	}
+	while (value--)
+	{
+		longList = longList->next;
+	}
+	while (longList && shortList)
+	{
+		if (longList->val == shortList->val)
+		{
+			return longList;
+		}
+		else
+		{
+			longList = longList->next;
+			shortList = shortList->next;
+		}
+	}
+	return NULL;
 }
-
-
-
 
 
 
