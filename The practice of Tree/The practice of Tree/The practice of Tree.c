8dc3eb22fecc5c3e2e17f5640c17a6a54efad812
Ellyn-jia <15846706868@163.com>
2020-03-12 15:35:04 +0800

    3.12

--- The practice of Tree/The practice of Tree/The practice of Tree.c
+++ The practice of Tree/The practice of Tree/The practice of Tree.c
@@ -79,3 +79,68 @@ bool isBalanced(struct TreeNode* root){
 }
 
 
+//翻转二叉树
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+
+
+struct TreeNode* invertTree(struct TreeNode* root){
+	if (root == NULL)
+		return NULL;
+	struct TreeNode* t = root->left;
+	root->left = root->right;
+	root->right = t;
+
+	invertTree(root->left);
+	invertTree(root->right);
+	return root;
+}
+
+
+//二叉树的最大深度
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+
+
+int maxDepth(struct TreeNode* root){
+	if (root == NULL)
+		return 0;
+	int leftDepth = maxDepth(root->left);
+	int rightDepth = maxDepth(root->right);
+	return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+}
+
+
+//判断单值二叉树
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     struct TreeNode *left;
+*     struct TreeNode *right;
+* };
+*/
+
+
+bool isUnivalTree(struct TreeNode* root){
+	if (root == NULL)
+		return true;
+	if (root->left && root->left->val != root->val)
+		return false;
+	if (root->right && root->right->val != root->val)
+		return false;
+	return isUnivalTree(root->left) && isUnivalTree(root->right);
+
+}
